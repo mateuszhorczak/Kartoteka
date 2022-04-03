4a10741f30bc6305f321b1388ef77e7e62ce5897
mhorczak <82621@student.pb.edu.pl>
2022-04-03 17:05:30 +0100

    tak

--- src/com/github/mateuszhorczak/Main.java
+++ src/com/github/mateuszhorczak/Main.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main (String[] args) {
         Kartoteka kartoteka = new Kartoteka(new ArrayList<Osoba>());
-        Osoba os = new Osoba("Jank", "Janowski");
-        kartoteka.dodaj(new Osoba("Jan", "Janowski"));
-        kartoteka.dodaj(new Osoba("Jana", "Janowski"));
+        Osoba os = new Osoba("Stefan", "Stefanski");
+        kartoteka.dodaj(new Osoba("Maciej", "Maciejewski"));
+        kartoteka.dodaj(new Osoba("Halina", "Halinowska"));
         kartoteka.dodaj(os);
         kartoteka.print();
         kartoteka.usun(os);
--- src/com/github/mateuszhorczak/kartoteka/mockup/Kartoteka.java
+++ src/com/github/mateuszhorczak/kartoteka/mockup/Kartoteka.java
@@ -2,7 +2,14 @@ package com.github.mateuszhorczak.kartoteka.mockup;
 
 import com.github.mateuszhorczak.kartoteka.Osoba;
 
+import java.util.List;
+
 public class Kartoteka implements com.github.mateuszhorczak.kartoteka.Kartoteka {
+    private List<Osoba> kartoteka;
+
+    public Kartoteka(List<Osoba> kartoteka) {
+        this.kartoteka = kartoteka;
+    }
 
     @Override
     public void dodaj(Osoba osoba) { }
@@ -25,4 +32,9 @@ public class Kartoteka implements com.github.mateuszhorczak.kartoteka.Kartoteka 
         return new Osoba("Gall", "Anonim");
     }
 
+    public void print() {
+        for (var item : this.kartoteka) {
+            System.out.println(item);
+        }
+    }
 }
